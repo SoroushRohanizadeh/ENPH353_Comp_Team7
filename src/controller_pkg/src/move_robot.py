ec3227b5c0342a0596764d6f75b9528ecbdba659
jacob <jts.simms@gmail.com>
2024-11-18 18:06:26 -0800

    start and stop working

--- src/controller_pkg/src/move_robot.py
+++ src/controller_pkg/src/move_robot.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2 as cv
 import numpy as np
@@ -15,17 +16,34 @@ class ControlNode:
         self.bridge = CvBridge()
         self.move = Twist()
 
-        self.pub = rospy.Publisher('B1/cmd_vel', Twist, queue_size=1)
+        self.pub1 = rospy.Publisher('B1/cmd_vel', Twist, queue_size=1)
+        self.pub2 = rospy.Publisher('score_tracker', String, queue_size=1)
+
         self.rate = rospy.Rate(1)
 
-        self.move.linear.x = 0.5
-        self.pub.publish(self.move)
+        while not rospy.Time.now().to_sec() > 0:
+            rospy.sleep(0.1)
 
+        rospy.sleep(1.0)
+
+        self.pub2.publish("Team7,password,0,NA")
+
+        rospy.sleep(1.0)
+
+        self.move.linear.x = 1
+        self.pub1.publish(self.move)
+
+        rospy.sleep(5.0)
+
+        self.move.linear.x = 0
+        self.pub1.publish(self.move)
+
+        self.pub2.publish("Team7,password,-1,NA")
     
 
     def run(self):
         while not rospy.is_shutdown():
-            self.pub.publish(self.move)
+            self.pub1.publish(self.move)
             self.rate.sleep()
 
 if __name__ == '__main__':
