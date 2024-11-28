e7b1c3454ff707ccc5b0009dafb52ed3c9ced26f
SoroushRohanizadeh <rohanisoroush@gmail.com>
2024-11-28 15:19:06 -0800

    pub messages to viewer

--- src/controller_pkg/src/clue_board/clue_board.py
+++ src/controller_pkg/src/clue_board/clue_board.py
@@ -1,4 +1,33 @@
-import cv2 as cv
+import cv2
 
 def detectClueBoard(img):
-    return str()
+    img = preProcess(img)
+    if not boardDetected(img):
+        return False, 0, str()
+    return True, parseBoard(img)
+
+def detectClueBoard_Debug(img):
+    '''
+    used only during development
+    '''
+    return preProcess(img)
+
+def parseBoard(img):
+    '''
+    Parse board for message and number
+    @returns board number, message
+    '''
+    return 0, str()
+
+def boardDetected(img):
+    '''
+    Look for a clue board in img
+    @return True if board detected
+    '''
+    return False
+
+def preProcess(img):
+    '''
+    reduce dimensionality of img
+    '''
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- src/controller_pkg/src/move_robot.py
+++ src/controller_pkg/src/move_robot.py
@@ -8,6 +8,9 @@ from cv_bridge import CvBridge
 
 import clue_board.clue_board as cb
 import line_follow.line_follow as lf
+import cv2
+
+DEBUG = True
 
 class ControlNode:
 
@@ -20,34 +23,42 @@ class ControlNode:
 
         self.cmd_vel = rospy.Publisher('B1/cmd_vel', Twist, queue_size = 1)
         self.score_tracker = rospy.Publisher('/score_tracker', String, queue_size = 1)
-        self.cam = rospy.Subscriber('/B1/pi_camera/image_raw', Image, self.cameraCallback)
+        self.cam = rospy.Subscriber('/B1/rrbot/camera1/image_raw', Image, self.cameraCallback)
         self.rate = rospy.Rate(1)
+        if DEBUG:
+            self.debug = rospy.Publisher('/image_debug', Image, queue_size = 1)
 
     def run(self):
 
         while not rospy.Time.now().to_sec() > 0:
             rospy.sleep(0.1)
 
-        rospy.sleep(1.0)
-        self.startTimer()
-        rospy.sleep(1.0)
-        self.setMotion(1)
-        self.cmd_vel.publish(self.moveCommand)
+        # rospy.sleep(1.0)
+        # self.startTimer()
+        # rospy.sleep(1.0)
+        # self.setMotion(1)
+        # self.cmd_vel.publish(self.moveCommand)
 
-        rospy.sleep(5.0)
-        self.stop()
-        self.cmd_vel.publish(self.moveCommand)
-        self.stopTimer()
+        # rospy.sleep(5.0)
+        # self.stop()
+        # self.cmd_vel.publish(self.moveCommand)
+        # self.stopTimer()
 
         while not rospy.is_shutdown():
             self.cmd_vel.publish(self.moveCommand) # should only be called here
             self.rate.sleep()
 
     def cameraCallback(self, img):
-        self.setMotion(lf.line_follow(img))
-        ret, num, msg = cb.detectClueBoard(self.bridge.imgmsg_to_cv2(img, desired_encoding='bgr8'))
-        if (ret):
-            self.publishClue(num, msg)
+        x, yaw = lf.line_follow(img)
+        self.setMotion(x, yaw)
+
+        if DEBUG:
+            img = cb.detectClueBoard_Debug(self.bridge.imgmsg_to_cv2(img, desired_encoding='bgr8'))
+            self.debug.publish(self.bridge.cv2_to_imgmsg(img))
+        else:
+            ret, num, msg = cb.detectClueBoard(self.bridge.imgmsg_to_cv2(img, desired_encoding='bgr8'))
+            if (ret):
+                self.publishClue(num, msg)
 
     def publishClue(self, num, msg):
         self.score_tracker.publish("Team7,password," + num + "," + msg)
@@ -59,16 +70,16 @@ class ControlNode:
         self.score_tracker.publish("Team7,password,-1,NA")
 
     def setMotion(self, x, yaw = 0):
-        self.moveCommand.linear.x = x
-        self.moveCommand.angular.z = yaw
+        self.moveCommand.linear.x = float(x)
+        self.moveCommand.angular.z = float(yaw)
 
     def stop(self):
         self.setMotion(0, 0)
 
+
 if __name__ == '__main__':
     try:
         node = ControlNode()
-        print("node init")
         node.run()
         rospy.spin()
     except rospy.ROSInterruptException:
