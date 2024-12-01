ecdf9e69aceb0d30106be3b7d695514c5f66445c
jacob <jts.simms@gmail.com>
2024-12-01 15:03:51 -0800

    you can stick the res trust

--- src/controller_pkg/src/move_robot.py
+++ src/controller_pkg/src/move_robot.py
@@ -5,6 +5,8 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge
+from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.srv import SetModelState
 
 from clue_board.clue_board import ClueBoard
 import line_follow.line_follow as lf
@@ -80,6 +82,26 @@ class ControlNode:
     def stop(self):
         self.setMotion(0, 0)
 
+    def respawn(self, position):
+
+        msg = ModelState()
+        msg.model_name = 'B1'
+
+        msg.pose.position.x = position[0]
+        msg.pose.position.y = position[1]
+        msg.pose.position.z = position[2]
+        msg.pose.orientation.x = position[3]
+        msg.pose.orientation.y = position[4]
+        msg.pose.orientation.z = position[5]
+        msg.pose.orientation.w = position[6]
+
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            resp = set_state(msg)
+
+        except rospy.ServiceException:
+            print ("Service call failed")
 
 if __name__ == '__main__':
     try:
