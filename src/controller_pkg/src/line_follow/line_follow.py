996f815967efbeca5648906f3912fc149345baf0
jacob <jts.simms@gmail.com>
2024-12-01 14:23:56 -0800

    no more hitting andy

--- src/controller_pkg/src/line_follow/line_follow.py
+++ src/controller_pkg/src/line_follow/line_follow.py
@@ -2,10 +2,14 @@ import cv2 as cv
 import numpy as np
 import rospy
 import math
+from collections import deque
 
 def line_follow(self, img):
 
     image = self.bridge.imgmsg_to_cv2(img, desired_encoding="bgr8")
+
+    if detect_crosswalk(image):
+        return 0,0
 
     filtered = line_img_filter(image)
 
@@ -13,16 +17,16 @@ def line_follow(self, img):
 
     return compute_twist(x_target,y_target)
 
-def line_img_filter(img):
+lower_bound = np.array([240,240,240])
+upper_bound = np.array([255,255,255])
 
-    lower_bound = np.array([240,240,240])
-    upper_bound = np.array([255,255,255])
+def line_img_filter(img):
 
     mask = cv.inRange(img, lower_bound, upper_bound)
 
     result = cv.bitwise_and(img, img, mask=mask)
 
-    # cv.imshow('Original', image)
+    # cv.imshow('Original', img)
     # cv.imshow('Filtered', result)
     # cv.waitKey(0)
     # cv.destroyAllWindows()
@@ -54,9 +58,9 @@ def get_target_coord(img):
 
     return x_target,y_target  
 
-Kp_linear = 0.5
-Kp_angular = 2.0
-max_linear = 1.0  
+Kp_linear = 0.001
+Kp_angular = 1.0
+max_linear = 5.0  
 max_angular = 4.0 
 
 def compute_twist(x_target, y_target):
@@ -72,3 +76,35 @@ def compute_twist(x_target, y_target):
     ang_vel = max(min(Kp_angular * angle, max_angular), -max_angular)
     print(x_vel,ang_vel)
     return x_vel, ang_vel
+
+history = deque(maxlen=5)
+
+def acc_comms(x_vel, yaw): 
+
+    history.append((x_vel,yaw))
+
+    x_final = 0
+    yaw_final = 0
+
+    for comm in history:
+        x_final = x_final + comm[0]
+        yaw_final = yaw_final + comm[1]
+
+    return x_final/len(history),yaw_final/len(history)
+
+lower_red = np.array([0,0,200])
+upper_red = np.array([20,20,255])
+
+def detect_crosswalk(img):
+
+    row = img[600:650]
+    row = row.reshape(50, -1, 3)
+
+    red_mask = cv.inRange(row, lower_red, upper_red)
+
+    red_pixels = np.sum(red_mask>0)
+
+    if (red_pixels / 800) > 0.3:
+        return True
+
+    return False
