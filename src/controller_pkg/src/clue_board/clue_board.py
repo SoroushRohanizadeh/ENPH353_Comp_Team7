1484cba095dda3b359fa57161ec870a5d4c71234
SoroushRohanizadeh <rohanisoroush@gmail.com>
2024-12-01 23:51:38 -0800

    able to perspective transform the board from the full FOV of the robot

--- src/controller_pkg/src/clue_board/clue_board.py
+++ src/controller_pkg/src/clue_board/clue_board.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 def detectClueBoard(img):
-    img = preProcess(img)
     detected, transformed_img = detectBoard(img)
     if not detected:
         return False, 0, str()
@@ -12,7 +11,6 @@ def detectClueBoard_Debug(img):
     '''
     used only during development
     '''
-    # img = preProcess(img)
     detected, transformed_img = detectBoard(img)
     return transformed_img
 
@@ -49,8 +47,9 @@ def detectBoard(img):
 
         if len(approx) == 4:
             # cv2.polylines(gray, [approx], isClosed = True, color=255, thickness = 3)
-            contour_corners = sort_corners(np.float32(approx))
-            
+            # contour_corners = np.float32(approx)
+            contour_corners = sort_corners(approx)
+
 
             w, h = gray.shape
             image_corners = np.float32([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]])
@@ -60,44 +59,15 @@ def detectBoard(img):
 
     return ret_img != img, ret_img
 
-def preProcess(img):
-    '''
-    reduce dimensionality of img
-    '''
-    return img
-
 def sort_corners(corners):
-    x0, y0 = corners[0][0]
-    x1, y1 = corners[1][0]
-    x2, y2 = corners[2][0]
-    x3, y3 = corners[3][0]
+    corners = [point[0] for point in corners]
+    sorted_corners = sorted(corners, key=lambda x: x[0])
+    
+    left = sorted_corners[:2]
+    right = sorted_corners[2:]
 
-    # Find the top-left, top-right, bottom-left, bottom-right by comparing x and y coordinates
-    if x0 < x1 and y0 < y1:
-        top_left = corners[0]
-        top_right = corners[1]
-    elif x0 > x1 and y0 < y1:
-        top_left = corners[1]
-        top_right = corners[0]
-    elif x0 < x1 and y0 > y1:
-        bottom_left = corners[0]
-        bottom_right = corners[1]
-    else:
-        bottom_left = corners[1]
-        bottom_right = corners[0]
+    left = sorted(left, key=lambda x: x[1])
+    right = sorted(right, key=lambda x: x[1])
+    
+    return np.array([left[0], right[0], right[1], left[1]], dtype=np.float32)
 
-    if x2 < x3 and y2 < y3:
-        bottom_left = corners[2]
-        bottom_right = corners[3]
-    elif x2 > x3 and y2 < y3:
-        bottom_left = corners[3]
-        bottom_right = corners[2]
-    elif x2 < x3 and y2 > y3:
-        top_left = corners[2]
-        top_right = corners[3]
-    else:
-        top_left = corners[3]
-        top_right = corners[2]
-
-    corners = [top_left, top_right, bottom_right, bottom_left]
-    return np.float32(corners)
